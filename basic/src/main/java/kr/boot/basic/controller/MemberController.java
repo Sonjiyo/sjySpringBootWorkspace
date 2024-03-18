aaa7cceddeed8087d8bde55c87bcface741dc58c
Sonjiyoung <147456475+Sonjiyo@users.noreply.github.com>
2024-03-18 17:24:06 +0900

    .

--- basic/src/main/java/kr/boot/basic/controller/MemberController.java
+++ basic/src/main/java/kr/boot/basic/controller/MemberController.java
@@ -1,9 +1,12 @@
 package kr.boot.basic.controller;
 
 import kr.boot.basic.domain.Member;
+import kr.boot.basic.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -11,15 +14,26 @@ import java.util.List;
 @RequestMapping("/members")
 @Controller
 public class MemberController {
-
+    @Autowired
+    MemberService memberService;
     @GetMapping("/new")
     public String creatingForm(){
         return "members/createMemberForm";
     }
 
+    @PostMapping("/new")
+    public String create(MemberForm form){
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/members";
+    }
+
     @GetMapping("")
     public String List(Model model){
-        List<Member> members = null;
+        List<Member> members = memberService.findMembers();
         model.addAttribute("members", members);
         return "members/memberList";
     }
--- basic/src/main/java/kr/boot/basic/servlet/BasicServlet.java
+++ basic/src/main/java/kr/boot/basic/servlet/BasicServlet.java
@@ -8,17 +8,26 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
-@WebServlet(name="basicServlet", urlPatterns = "/basic")
+@WebServlet(name="basicServlet" , urlPatterns = "/basic")
 public class BasicServlet extends HttpServlet {
-    //ctrl + o
+    // ctrl + o
 
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        System.out.println(" basic servlet ");
-        System.out.println("req = "+req );
-        System.out.println("resp = "+resp);
+        System.out.println(" basic servlet  ");
+        System.out.println("req = " + req);
+        System.out.println("resp = " + resp);
 
-        //
+        // 파라미터 값 읽어올때
+        String username = req.getParameter("username");
+        System.out.println("username = " + username);
+
+        // 응답해줄때
+        resp.setContentType("text/plain");
+        resp.setCharacterEncoding("utf-8");
+        resp.getWriter().write("hello " + username);
+
+
     }
 }
